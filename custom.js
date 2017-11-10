c1dd16875136a55f1c5fb4bca5af0e0b14e1aabb
Billy Lundh <billy.lundh@gmail.com>
2017-11-10 15:44:06 +0100

    asd

--- custom.js
+++ custom.js
@@ -7,4 +7,24 @@ var flkty = new Flickity( '.main-carousel', {
 });
 
 
+
+$(function(){
+
+              var $container = $('masonry');
+
+              var reMasonry = function() {
+                $container.masonry();
+              };
+
+             $('.item').click(function(){
+                /*var $this = $(this),
+                    size = $this.hasClass('large') ?
+                      { width: 290, height: 290 } :
+                      { width: 590, height: 590 };
+                $this.toggleClass('large').animate( size, reMasonry );
+                $grid.masonry();*/
+                console.log("hey");
+              });
+            });
+
     
