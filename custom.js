881e681512e46aad35d5a787bfcb22bfe8b9f62d
Billy Lundh <billy.lundh@gmail.com>
2017-11-10 09:23:55 +0100

    first test

---
+++ custom.js
@@ -0,0 +1,29 @@
+
+
+var flkty = new Flickity( '.main-carousel', {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+
+
+$(function(){
+
+              var $container = $('masonry');
+
+              var reMasonry = function() {
+                $container.masonry();
+              };
+
+             $('.item').click(function(){
+                var $this = $(this),
+                    size = $this.hasClass('large') ?
+                      { width: 290, height: 290 } :
+                      { width: 590, height: 590 };
+                $this.toggleClass('large').animate( size, reMasonry );
+                $grid.masonry();
+              });
+            });
+
+    
