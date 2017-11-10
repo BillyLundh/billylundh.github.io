44069fabcec276abb623008353e6e810b80cb281
Billy Lundh <billy.lundh@gmail.com>
2017-11-10 15:40:30 +0100

    asd

--- custom.js
+++ custom.js
@@ -17,12 +17,7 @@ $(function(){
               };
 
              $('.item').click(function(){
-                var $this = $(this),
-                    size = $this.hasClass('large') ?
-                      { width: 290, height: 290 } :
-                      { width: 590, height: 590 };
-                $this.toggleClass('large').animate( size, reMasonry );
-                $grid.masonry();
+                console.log("hey");
               });
             });
 
