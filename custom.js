904ac1ebc50e39fa61329ff29647cc8f8407defa
Billy Lundh <billy.lundh@gmail.com>
2017-11-16 13:12:39 +0100

    asdas

--- custom.js
+++ custom.js
@@ -1,66 +1,10 @@
-
+<script>
 $('.main-carousel').flickity({
   // options
   cellAlign: 'left',
   contain: true
 });
+</script>
 
 
 
-/*
-$(function(){
-
-              var $container = $('masonry');
-
-              var reMasonry = function() {
-                $container.masonry();
-              };
-
-             $('.item').click(function(){
-                /*var $this = $(this),
-                    size = $this.hasClass('large') ?
-                      { width: 290, height: 290 } :
-                      { width: 590, height: 590 };
-                $this.toggleClass('large').animate( size, reMasonry );
-                $grid.masonry();
-                console.log("hey");
-              });
-            });
-*/
-
-/*
-$(function(){
-
-              $('.main-carousel').flickity({
-                // options
-                cellAlign: 'left',
-                contain: true
-              });
-/*
-              var $container = $('#masonry');
-
-
-              // trigger masonry
-              var trigger = function() {
-                $container.masonry({
-                columnWidth: 300,
-                animate: true
-              });
-              };
-
-              var reMasonry = function() {
-                $container.masonry();
-              };
-
-
-
-              $('.item').click(function(){
-                var $this = $(this),
-                    size = $this.hasClass('large') ?
-                      { width: 245, height: 290 } :
-                      { width: 245, height: 590 };
-                $this.toggleClass('large').animate( size, reMasonry );
-                console.log("hey");
-              });
-            });*/
-            
